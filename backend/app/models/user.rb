c7ba58058872817ff6786c28d10931995b427c8a
Mitchell <mitchell.alton@live.com>
2020-04-27 14:46:36 -0500

    added roles to user class

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    enum role: [:gaurdian, :child]
+
     has_many :chores
     has_many :rewards
     has_one :credit_score
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_180159) do
+ActiveRecord::Schema.define(version: 2020_04_27_194028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_180159) do
     t.integer "family_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
   end
 
   create_table "wallets", force: :cascade do |t|
